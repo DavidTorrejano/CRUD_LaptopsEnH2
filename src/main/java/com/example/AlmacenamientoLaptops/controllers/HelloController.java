f820b109f82bd9f2b3466329638a696647689873
David Torrejano <davitorrejano@gmail.com>
2023-06-22 21:32:17 -0500

    Hello controller hidden

--- src/main/java/com/example/AlmacenamientoLaptops/controllers/HelloController.java
+++ src/main/java/com/example/AlmacenamientoLaptops/controllers/HelloController.java
@@ -1,9 +1,11 @@
 package com.example.AlmacenamientoLaptops.controllers;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Hidden
 public class HelloController {
 
     @GetMapping("/saludo")
